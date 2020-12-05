131098897b00ec132271671192834804e1331ecc
blissful <blissful@sunsetglow.net>
2020-12-05 00:23:40 -0500

    d5 crap race haha

--- scratch/run.py
+++ scratch/run.py
@@ -5,8 +5,6 @@ import re
 from functools import reduce
 from pathlib import Path
 
-from aocd import submit  # type: ignore
-
 YEAR = 2020
 DAY = 3
 
@@ -35,15 +33,46 @@ try:
 except ValueError:
     pass
 
-# TODO: SOLUTION HERE
-
 
 def part1():
-    pass
+    ids = []
+    for line in lines:
+        low = 0
+        high = 127
+        collow = 0
+        colhigh = 7
+        print(line)
+        for c in line:
+            if c == "F":
+                high = (high + low) // 2
+            elif c == "B":
+                low = (high + low) // 2 + 1
+            elif c == "R":
+                collow = (colhigh + collow) // 2 + 1
+            elif c == "L":
+                colhigh = (colhigh + collow) // 2
+            print(low, high, collow, colhigh)
+
+        seat_id = low * 8 + collow
+        print(seat_id)
+        ids.append(seat_id)
+
+    return ids
 
 
 def part2():
-    pass
+    ids = part1()
+    print(sorted(ids))
+    max_ = max(ids)
+    low_ = min(ids)
+    print(max_, low_)
+    print("PRINTING")
+    for i in range(low_, max_):
+        if i not in ids and i - 1 in ids and i + 1 in ids:
+            print(i)
+            # return i
+
+    return max_
 
 
 print(part1())

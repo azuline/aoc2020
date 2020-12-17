0127c3161a13283abc9b8c9adf5a3f91d239620d
blissful <blissful@sunsetglow.net>
2020-12-17 17:26:03 -0500

    refactor day17 to be less crap

--- rust/src/day17.rs
+++ rust/src/day17.rs
@@ -2,6 +2,9 @@ use itertools::Itertools;
 use std::collections::HashMap;
 
 // Ugh this whole 3D/4D thing is a mess. I don't want to think about it.
+//
+// We're just doing dependency injection. 3D and 4D traits, wrapper class over the grid, differing
+// functions split up in implementations of the traits.
 
 static INPUT: &str = include_str!("../../inputs/day17.txt");
 const THREE_ADJACENTS: [i32; 3] = [-1, 0, 1];
@@ -9,81 +12,14 @@ const THREE_ADJACENTS: [i32; 3] = [-1, 0, 1];
 // (x, y, z, w)
 type Coord = (i32, i32, i32, i32);
 
-// Dimensions are `[min, max)` (max-val is exclusive).
-#[derive(Clone)]
-struct Dimensions {
-    x: (i32, i32),
-    y: (i32, i32),
-    z: (i32, i32),
-    w: (i32, i32),
-}
-
-impl IntoIterator for Dimensions {
-    type Item = Coord;
-    type IntoIter = std::vec::IntoIter<Self::Item>;
-
-    fn into_iter(self) -> Self::IntoIter {
-        let mut coords = Vec::new();
-
-        for x in self.x.0..self.x.1 {
-            for y in self.y.0..self.y.1 {
-                for z in self.z.0..self.z.1 {
-                    for w in self.w.0..self.w.1 {
-                        coords.push((x, y, z, w));
-                    }
-                }
-            }
-        }
-
-        coords.into_iter()
-    }
-}
-
 // Map of coords to active (true) / inactive (false).
 type Grid = HashMap<Coord, bool>;
 
 trait ConwayGrid {
-    fn get_mutating_coords_3d(&self) -> Dimensions;
-    fn get_mutating_coords_4d(&self) -> Dimensions;
     fn get_dimensions(&self) -> Dimensions;
-    fn get_active_adjacent_count_3d(&self, coord: &Coord) -> u32;
-    fn get_active_adjacent_count_4d(&self, coord: &Coord) -> u32;
-    fn count_active(&self) -> u32;
 }
 
 impl ConwayGrid for HashMap<Coord, bool> {
-    fn get_mutating_coords_3d(&self) -> Dimensions {
-        let Dimensions {
-            x: old_x,
-            y: old_y,
-            z: old_z,
-            w: old_w,
-        } = self.get_dimensions();
-
-        Dimensions {
-            x: (old_x.0 - 1, old_x.1 + 1),
-            y: (old_y.0 - 1, old_y.1 + 1),
-            z: (old_z.0 - 1, old_z.1 + 1),
-            w: old_w,
-        }
-    }
-
-    fn get_mutating_coords_4d(&self) -> Dimensions {
-        let Dimensions {
-            x: old_x,
-            y: old_y,
-            z: old_z,
-            w: old_w,
-        } = self.get_dimensions();
-
-        Dimensions {
-            x: (old_x.0 - 1, old_x.1 + 1),
-            y: (old_y.0 - 1, old_y.1 + 1),
-            z: (old_z.0 - 1, old_z.1 + 1),
-            w: (old_w.0 - 1, old_w.1 + 1),
-        }
-    }
-
     fn get_dimensions(&self) -> Dimensions {
         let keys = self.keys().collect_vec();
         let x_min = keys.iter().map(|c| c.0).min().unwrap();
@@ -102,58 +38,151 @@ impl ConwayGrid for HashMap<Coord, bool> {
             w: (w_min, w_max),
         }
     }
+}
 
-    fn get_active_adjacent_count_3d(&self, coord: &Coord) -> u32 {
-        get_adjacent_cubes_3d(coord)
-            .iter()
-            .filter(|c| *self.get(c).unwrap_or(&false))
-            .count() as u32
+// Dimensions are `[min, max)` (max-val is exclusive).
+#[derive(Clone)]
+struct Dimensions {
+    x: (i32, i32),
+    y: (i32, i32),
+    z: (i32, i32),
+    w: (i32, i32),
+}
+
+impl IntoIterator for Dimensions {
+    type Item = Coord;
+    type IntoIter = std::vec::IntoIter<Self::Item>;
+
+    // TODO: How can I use iter() for this? Rather than do this crap vector stuff.
+    fn into_iter(self) -> Self::IntoIter {
+        let mut coords = Vec::new();
+
+        for x in self.x.0..self.x.1 {
+            for y in self.y.0..self.y.1 {
+                for z in self.z.0..self.z.1 {
+                    for w in self.w.0..self.w.1 {
+                        coords.push((x, y, z, w));
+                    }
+                }
+            }
+        }
+
+        coords.into_iter()
+    }
+}
+
+trait GridFns {
+    fn get_mutating_coords(&self, grid: &Grid) -> Dimensions;
+    fn get_adjacent_cubes(&self, coord: &Coord) -> Vec<Coord>;
+}
+
+#[derive(Clone)]
+struct ThreeDimensionFns {}
+
+impl GridFns for ThreeDimensionFns {
+    fn get_mutating_coords(&self, grid: &Grid) -> Dimensions {
+        let Dimensions {
+            x: old_x,
+            y: old_y,
+            z: old_z,
+            w: old_w,
+        } = grid.get_dimensions();
+
+        Dimensions {
+            x: (old_x.0 - 1, old_x.1 + 1),
+            y: (old_y.0 - 1, old_y.1 + 1),
+            z: (old_z.0 - 1, old_z.1 + 1),
+            w: old_w,
+        }
     }
 
-    fn get_active_adjacent_count_4d(&self, coord: &Coord) -> u32 {
-        get_adjacent_cubes_4d(coord)
+    fn get_adjacent_cubes(&self, (x, y, z, w): &Coord) -> Vec<Coord> {
+        THREE_ADJACENTS
             .iter()
-            .filter(|c| *self.get(c).unwrap_or(&false))
+            .combinations_with_replacement(3)
+            .filter(|adj| *adj[0] != 0 || *adj[1] != 0 || *adj[2] != 0)
+            .flat_map(|c| c.into_iter().permutations(3).collect_vec())
+            .unique()
+            .map(|adj| (x + *adj[0], y + *adj[1], z + *adj[2], *w))
+            .collect()
+    }
+}
+
+#[derive(Clone)]
+struct FourDimensionFns {}
+
+impl GridFns for FourDimensionFns {
+    fn get_mutating_coords(&self, grid: &Grid) -> Dimensions {
+        let Dimensions {
+            x: old_x,
+            y: old_y,
+            z: old_z,
+            w: old_w,
+        } = grid.get_dimensions();
+
+        Dimensions {
+            x: (old_x.0 - 1, old_x.1 + 1),
+            y: (old_y.0 - 1, old_y.1 + 1),
+            z: (old_z.0 - 1, old_z.1 + 1),
+            w: (old_w.0 - 1, old_w.1 + 1),
+        }
+    }
+
+    fn get_adjacent_cubes(&self, (x, y, z, w): &Coord) -> Vec<Coord> {
+        THREE_ADJACENTS
+            .iter()
+            .combinations_with_replacement(4)
+            .filter(|adj| *adj[0] != 0 || *adj[1] != 0 || *adj[2] != 0 || *adj[3] != 0)
+            .flat_map(|c| c.into_iter().permutations(4).collect_vec())
+            .unique()
+            .map(|adj| (x + *adj[0], y + *adj[1], z + *adj[2], w + *adj[3]))
+            .collect()
+    }
+}
+
+#[derive(Clone)]
+struct Data<T: GridFns + Clone> {
+    grid: Grid,
+    fns: T,
+}
+
+impl<T: GridFns + Clone> Data<T> {
+    fn get_mutating_coords(&self) -> Dimensions {
+        self.fns.get_mutating_coords(&self.grid)
+    }
+
+    fn get_active_adjacent_count(&self, coord: &Coord) -> u32 {
+        self.fns
+            .get_adjacent_cubes(coord)
+            .iter()
+            .filter(|c| *self.grid.get(c).unwrap_or(&false))
             .count() as u32
     }
 
     fn count_active(&self) -> u32 {
-        self.get_dimensions()
+        self.grid
+            .get_dimensions()
             .into_iter()
-            .filter(|c| *self.get(c).unwrap_or(&false))
+            .filter(|c| *self.grid.get(c).unwrap_or(&false))
             .count() as u32
     }
-}
-
-/// Given a coordinate in the 3D grid, return the 26 adjacent coordinates.
-fn get_adjacent_cubes_3d((x, y, z, w): &Coord) -> Vec<Coord> {
-    THREE_ADJACENTS
-        .iter()
-        .combinations_with_replacement(3)
-        .filter(|adj| *adj[0] != 0 || *adj[1] != 0 || *adj[2] != 0)
-        .flat_map(|c| c.into_iter().permutations(3).collect_vec())
-        .unique()
-        .map(|adj| (x + *adj[0], y + *adj[1], z + *adj[2], *w))
-        .collect()
-}
-
-/// Given a coordinate in the 3D grid, return the 26 adjacent coordinates.
-fn get_adjacent_cubes_4d((x, y, z, w): &Coord) -> Vec<Coord> {
-    THREE_ADJACENTS
-        .iter()
-        .combinations_with_replacement(4)
-        .filter(|adj| *adj[0] != 0 || *adj[1] != 0 || *adj[2] != 0 || *adj[3] != 0)
-        .flat_map(|c| c.into_iter().permutations(4).collect_vec())
-        .unique()
-        .map(|adj| (x + *adj[0], y + *adj[1], z + *adj[2], w + *adj[3]))
-        .collect()
 }
 
 pub fn run() {
     let grid = transform_input(INPUT);
 
-    println!("Part 1: {}", part1(grid.clone()));
-    println!("Part 2: {}", part2(grid));
+    let data_3d = Data {
+        grid: grid.clone(),
+        fns: ThreeDimensionFns {},
+    };
+
+    println!("Part 1: {}", six_rounds(data_3d));
+
+    let data_4d = Data {
+        grid,
+        fns: FourDimensionFns {},
+    };
+    println!("Part 2: {}", six_rounds(data_4d));
 }
 
 fn transform_input(input: &'static str) -> Grid {
@@ -169,44 +198,23 @@ fn transform_input(input: &'static str) -> Grid {
         .collect()
 }
 
-fn part1(mut grid: Grid) -> u32 {
-    let mut old_grid = grid.clone();
+fn six_rounds<T: GridFns + Clone>(mut data: Data<T>) -> u32 {
+    let mut old_data = data.clone();
 
     for _ in 0..6 {
-        for coord in grid.get_mutating_coords_3d() {
-            let active_adjacent = old_grid.get_active_adjacent_count_3d(&coord);
-            let active = *old_grid.get(&coord).unwrap_or(&false);
+        for coord in old_data.get_mutating_coords() {
+            let active_adjacent = old_data.get_active_adjacent_count(&coord);
+            let active = *old_data.grid.get(&coord).unwrap_or(&false);
 
             if active && !(active_adjacent == 2 || active_adjacent == 3) {
-                grid.insert(coord, false);
+                data.grid.insert(coord, false);
             } else if !active && active_adjacent == 3 {
-                grid.insert(coord, true);
+                data.grid.insert(coord, true);
             }
         }
 
-        old_grid = grid.clone();
+        old_data = data.clone();
     }
 
-    grid.count_active()
-}
-
-fn part2(mut grid: Grid) -> u32 {
-    let mut old_grid = grid.clone();
-
-    for _ in 0..6 {
-        for coord in grid.get_mutating_coords_4d() {
-            let active_adjacent = old_grid.get_active_adjacent_count_4d(&coord);
-            let active = *old_grid.get(&coord).unwrap_or(&false);
-
-            if active && !(active_adjacent == 2 || active_adjacent == 3) {
-                grid.insert(coord, false);
-            } else if !active && active_adjacent == 3 {
-                grid.insert(coord, true);
-            }
-        }
-
-        old_grid = grid.clone();
-    }
-
-    grid.count_active()
+    data.count_active()
 }
